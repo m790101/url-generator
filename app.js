bfd40718159a5da3a53bd929dc6891d7fab5ce85
m790101 <m790101@hotmail.com>
2022-07-09 16:41:13 +0800

    Add redirect funtion

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
 const app = express()
-
-
+const Urls = require("./models/url")
+const db = mongoose.connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
+
 
 db.on("error",() => {
     console.log("mongodb error")
@@ -20,12 +20,38 @@ db.once("open", () => {
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
-app.use(express,)
+app.use(express.urlencoded({ extended: true }))
 
 
 
 app.get("/",(req,res) => {
     res.render("index")
+})
+
+app.post("/", (req,res) => {
+    const urlOrigin = req.body.url
+    console.log(req.body.url)
+    Urls.create({
+        urlOrigin:urlOrigin,
+        urlAfter: "abc"
+    })
+    .then(res.redirect("/"))
+})
+
+app.get("/:simple",(req,res) => {
+    const simple = req.params.simple
+    console.log(req.params.simple)
+
+    return Urls.find({ 'urlAfter': simple })
+    .lean()
+    .then((item) => {
+        res.redirect(item[0].urlOrigin)
+    })
+    .catch(err => console.log(err))
+    /*return Urls.find()
+    .lean()
+    .then(res.redirect(req.params.urlOrigin))
+    .catch(err => console.log(err))*/
 })
 
 app.listen(3000,() => {
--- models/url.js
+++ models/url.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const urlSchema = new Schema({
-    urlOrigin:{type:"string", require: true},
+    urlOrigin:{type:"String", require: true},
     urlAfter:{type:"String", require: true}
 })
 
